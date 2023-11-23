767f7164d98a12634c247ed398b24cdb84a04df6
LamiDs <c_rux@naver.com>
2023-11-23 11:11:24 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/heropy';
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'LamiDs / ByunKyungHwan',
+  email: 'sprtmsdy1@gmail.com',
+  blog: 'https://naver.com',
+  github: 'https://github.com',
+  repository: 'https://github.com',
+});
